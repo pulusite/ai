9ae4a51ddc39ed93dea752f3886c6b9d0a44291a
pulusite <pulusite@126.com>
2017-11-02 15:38:23 +0800

    update

---
+++ leet113.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+     def pathSum(self, root, sum):
+         """
+         :type root: TreeNode
+         :type sum: int
+         :rtype: List[List[int]]
+         """
+         def dfs(root, currsum, valuelist):
+             if root.left == None and root.right == None:
+                 if currsum == sum: res.append(valuelist)
+             if root.left:
+                 dfs(root.left, currsum+root.left.val, valuelist+[root.left.val])
+             if root.right:
+                 dfs(root.right, currsum+root.right.val,valuelist+[root.right.val])
+         
+         res = []
+         if root ==None: return res
+         dfs(root, root.val, [root.val])
+         return res
